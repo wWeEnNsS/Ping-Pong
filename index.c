a2625b319bb78024e5378452cf27e0740d9d79ac
Ali <afdsrxd@gmail.com>
2024-09-09 15:09:31 +0300

    clearing and displaying maps

--- index.c
+++ index.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
+#define mapWidth 65
+#define mapHeight 25
+
+char mas[mapHeight][mapWidth+1];
+
+typedef struct sRacket
+{
+    int x, y;
+}TRacket;
+
+void ClearMap()
+{
+    for (int i = 0; i < mapWidth; i++)
+    {
+        
+    }
+}
+
+void ShowMap()
+{
+    mas[mapHeight-1][mapWidth-1] = '\0';
+}
+
 int main()
 {
-    printf("Hello World");
+    
     return 0;
 }
