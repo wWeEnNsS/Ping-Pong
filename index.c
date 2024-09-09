18164fb3d5de14fc621a15310f6a8b38ea9c121d
Ali <afdsrxd@gmail.com>
2024-09-09 15:27:51 +0300

    Show map void function

--- index.c
+++ index.c
@@ -14,17 +14,34 @@ void ClearMap()
 {
     for (int i = 0; i < mapWidth; i++)
     {
-        
+        mas[0][i] = '#';
+    }
+    mas[0][mapWidth] = '\0';
+
+    strncpy(mas[1], mas[0], mapWidth+1);
+    for (int i = 1; i < mapWidth-1; i++)
+    {
+        mas[1][i] = ' ';
+    }
+
+    for (int i = 2; i < mapHeight; i++)
+    {
+        strncpy(mas[i], mas[1], mapWidth+1);
     }
 }
 
 void ShowMap()
 {
-    mas[mapHeight-1][mapWidth-1] = '\0';
+  //mas[mapHeight-1][mapWidth-1] = '\0';
+    for (int i = 0; i < mapHeight; i++)
+    {
+        printf("%s\n", mas[i]);
+    }
 }
 
 int main()
 {
-    
+    ClearMap();
+    ShowMap();
     return 0;
 }
